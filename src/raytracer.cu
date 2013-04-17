6408e93dd2967d04807ebdd1dea9ad9ea96c3380
Ian Stewart <iastewar@student.umass.edu>
2013-04-17 13:13:46 -0400

    Updated raytracer.cu

--- src/raytracer.cu
+++ src/raytracer.cu
@@ -11,47 +11,108 @@
 //Include GLFW
 #include <GL/glfw.h>
 
+//Include X
+#include <X11/X.h>
+#include <X11/Xlib.h>
+
+//Include other OpenGL
+#include <GL/gl.h>
+#include <GL/glx.h>
+#include <GL/glu.h>
+
 //Include project headers
-//#include <./raytraceutils.h>
-//#include <./raystructs.h>
+#include <./raytraceutils.h>
+#include <./raystructs.h>
 
 int mouse_old_x, mouse_old_y;//Old mouse position
 
 const unsigned int win_height = 640;//Window dimensions
 const unsigned int win_width = 480;
 
-int main(){
-	if(!glfwInit()) {
-		fprintf(stderr, "Failed to intialize GLFW\n");
-		return -1;
-	}
+Display 		*dpy;
+Window 			root;
+GLint att[] = 		{GLX_RGBA, GLX_DEPTH_SIZE,24,GLX_DOUBLEBUFFER, None};
+XVisualInfo		*vi;
+Colormap 		cmap;
+XSetWindowAttributes 	swa;
+Window 			win;
+GLXContext 		glc;
+XwindowAttributes 	gwa;
+XEvent 			xev;
 
-	glfwOpenWindowHint(GLFW_FSAA_SAMPLES, 4); // 4x antialiasing
-	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3); // We want OpenGL 3.3
-	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 3);
-	glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL
+int main(int argc, char *argv[]){
+	setUpXScreen();
 	
-	// Open a window and create its OpenGL context
-	if( !glfwOpenWindow( win_width, win_height, 0,0,0,0, 32,0, GLFW_WINDOW ) ) {
-		fprintf( stderr, "Failed to open GLFW window\n" );
-		glfwTerminate();
-		return -1;
+	while(1){
+	XNextEvent(dpy, &xev);
+	if(xev.type == Expose) {
+		XGetWindowAttributes(dpy, win, &gwa);
+		glViewport(0, 0, gwa.width, gwa.height);
+		DrawAQuad(); 
+		glXSwapBuffers(dpy, win);
+	
+		
+	} else if(xev.type == KeyPress) {
+		glXMakeCurrent(dpy, None, NULL);
+		glXDestroyContext(dpy, glc);
+		XDestroyWindow(dpy, win);
+		XCloseDisplay(dpy);
+		exit(0);
 	}
-	
-	// Initialize GLEW
-	glewExperimental=true; // Needed in core profile
-	if (glewInit() != GLEW_OK) {
-		fprintf(stderr, "Failed to initialize GLEW\n");
-		return -1;
-	}
-	glfwSetWindowTitle( "Tutorial 01" );
-	
-	//Ensure we can capture the excape key being pressed
-	glfewEnable(GLFW_STICKY_KEYS);
-	
-	do{
-		//Draw nothing
-		glfwSwapBuffers();
-	} while (glfwGetKey(GLFW_KEY_ESC) != GLFW_PRESS && glfwGetWindowParam(GLFW_OPENED);
+	}//End while(1)
+}
 
+//Sets up the X screen so the main drawing loop can run
+void setUpXScreen(){
+	//Attempt to get a new display
+	dpy = XOpenDisplay(NULL);
+	if(dpy == NULL){
+		printf("\n Cannot connect to X server\n");
+		exit(0);
+	}
+	
+	root = DefaultRootWindow(dpy);
+	vi = glXChooseVisual(dpy, 0,att);
+	if(vi == NULL){
+		printf("\n No appropriate visual found\n);
+		exit(0);
+	} else {
+		printf("\n Visual %p selected \n", (void *)vi->visualid);
+	}
+	
+	cmap = XCreateColormap(dpy,root, vi->visual, AllocNone);
+	
+	swa.colormap = cmap;
+	swa.event_mask = exposureMask | KeyPressMask;
+	
+	win = XCreateWindow(dpy, root, 0, 0, win_width, win_height, InputOutput, vi->visual, CWColormap | CWEventMask, &swa);
+	
+	XMapWindow(dpy, win);
+	XStoreName(dpy, win, "CUDA Ray Tracer - Ian Stewart & Alexander Newman");
+	
+	glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
+	glXMakeCurrent(dpy, win, glc);
+	
+	glEnable(GL_DEPTH_TEST);//May not need this for ray tracer	
+}//End setUpXScreen
+
+//Just a temporary thing. Will be replaced with code to draw objects
+//Drawing will be done by CUDA
+void DrawAQuad(){
+	glClearColor(1.0,1.0,1.0,1.0);
+	 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1., 1., -1., 1., 1., 20.);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0., 0., 10., 0., 0., 0., 0., 1., 0.);
+
+	glBegin(GL_QUADS);
+		glColor3f(1., 0., 0.); glVertex3f(-.75, -.75, 0.);
+		glColor3f(0., 1., 0.); glVertex3f( .75, -.75, 0.);
+		glColor3f(0., 0., 1.); glVertex3f( .75,  .75, 0.);
+		glColor3f(1., 1., 0.); glVertex3f(-.75,  .75, 0.);
+	glEnd();
 }
